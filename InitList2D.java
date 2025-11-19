f2f266b7ace1aeef4e962866a749b853b01305e3
BrandenP-byte <pham4878@mydusd.org>
2025-11-19 13:49:46 -0800

    Add files via upload

---
+++ InitList2D.java
@@ -0,0 +1,19 @@
+public class InitList2D
+{
+  public static void main(String[] args)
+  {
+    String[][] board = { {"close", "quarter"},
+                        {"moon", "rock"},
+                        {"band", "stand"},
+                        {"out", "shine"}  }; 
+String[][] board =
+  {{"close", "quarter"},{"moon", "rock"},{"band", "stand"},{"out", "shine"}};
+  for (int i = 0; i < 4; i++)
+
+{
+
+    System.out.println(board[i]); // show object references
+
+}
+ }
+}
